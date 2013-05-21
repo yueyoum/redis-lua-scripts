e97eb9d17134d6c722594515cbee2b3e3d84f064
yueyoum <yueyoum@gmail.com>
2013-05-21 23:00:57 +0800

    README

--- insert_ranking_test.py
+++ insert_ranking_test.py
@@ -1,41 +1,59 @@
-import redis
-r = redis.Redis(port=6381)
+import sys
+from optparse import OptionParser
 
-with open('update_arean_rank.lua', 'r') as f:
+import redis
+
+parse = OptionParser()
+parse.add_option(
+    '-p', '--port',
+    type = 'int',
+    dest = 'port',
+    default = 6379,
+)
+
+
+options, args = parse.parse_args(sys.argv)
+
+r = redis.Redis(port=options.port)
+
+with open('insert_ranking.lua', 'r') as f:
     x = f.read()
 
-r.delete('aabb')
-r.delete('aahh')
+
+RANK_LIST_KEY = 'testl'
+RANK_HASH_KEY = 'testh'
+
+
+r.delete(RANK_LIST_KEY)
+r.delete(RANK_HASH_KEY)
 
 pipe = r.pipeline()
 uids = xrange(1, 10001)
 
 for index, uid in enumerate(uids):
-    pipe.rpush('aabb', uid)
-    pipe.hset('aahh', uid, index+1)
+    pipe.rpush(RANK_LIST_KEY, uid)
+    pipe.hset(RANK_HASH_KEY, uid, index+1)
 
 pipe.execute()
 
 
 def test(*args):
     for uid, rank in zip(args[::2], args[1::2]):
-        assert int( r.hget('aahh', uid) ) == rank
+        assert int( r.hget(RANK_HASH_KEY, uid) ) == rank
 
-print 'start test'
 
-test(1, 1, 1000, 1000)
+test(1, 1, 1000, 1000, 10000, 10000)
 
-r.eval(x, 2, 'aabb', 'aahh', 1000, 1)
+r.eval(x, 2, RANK_LIST_KEY, RANK_HASH_KEY, 1000, 1)
 test(1, 2, 1000, 1, 990, 991, 1001, 1001)
 
-r.eval(x, 2, 'aabb', 'aahh', 9000, 1000)
+r.eval(x, 2, RANK_LIST_KEY, RANK_HASH_KEY, 9000, 1000)
 test(1, 3, 1000, 2, 9000, 1, 990, 992, 1001, 1002, 10000, 10000)
 
-r.eval(x, 2, 'aabb', 'aahh', 10000, 1)
+r.eval(x, 2, RANK_LIST_KEY, RANK_HASH_KEY, 10000, 1)
 test(1, 4, 1000, 2, 990, 993, 10000, 3)
 
-print r.lrange('aabb', 0, 10)
 
-r.delete('aabb')
-r.delete('aahh')
+r.delete(RANK_LIST_KEY)
+r.delete(RANK_HASH_KEY)
 
